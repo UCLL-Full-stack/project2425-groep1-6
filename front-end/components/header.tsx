89d3352068af97800d6ec939f54a199a03afcffb
NielStroobantsUCLL <niel.stroobants@student.ucll.be>
2024-12-15 20:35:09 +0100

    styling fix

--- front-end/components/header.tsx
+++ front-end/components/header.tsx
@@ -5,38 +5,31 @@ const Header: React.FC = () => {
   return (
     <header className="p-3 mb-3 border-bottom bg-dark bg-gradient">
       <div className="d-flex flex-column align-items-center">
-        <a
-          className="fs-2 text-white-50 text-decoration-none mb-2"
-          style={{ fontFamily: "Cursive", textShadow: "2px 2px 4px #000000" }}
-        >
+        <a className="fs-2 text-white-50 text-decoration-none mb-2 font-cursive text-shadow">
           Bio-Scope
         </a>
-        <nav className="nav d-flex align-items-center">
+        <nav className="flex items-center">
           <Link
             href="/"
-            className="nav-link px-4 fs-5 text-white hover:text-yellow-400 hover:text-shadow-lg active:text-orange-500"
-            style={{ textShadow: "1px 1px 2px #000000" }}
+            className="px-4 text-lg text-white hover:text-yellow-400 hover:shadow-lg active:text-orange-500"
           >
             Home
           </Link>
           <Link
             href="/movies"
-            className="nav-link px-4 fs-5 text-white hover:text-yellow-400 hover:text-shadow-lg active:text-orange-500"
-            style={{ textShadow: "1px 1px 2px #000000" }}
+            className="px-4 text-lg text-white hover:text-yellow-400 hover:shadow-lg active:text-orange-500"
           >
             Movies
           </Link>
           <Link
             href="/login"
-            className="nav-link px-4 fs-5 text-white hover:text-yellow-400 hover:text-shadow-lg active:text-orange-500"
-            style={{ textShadow: "1px 1px 2px #000000" }}
+            className="px-4 text-lg text-white hover:text-yellow-400 hover:shadow-lg active:text-orange-500"
           >
             Login
           </Link>
           <Link
             href="/register"
-            className="nav-link px-4 fs-5 text-white hover:text-yellow-400 hover:text-shadow-lg active:text-orange-500"
-            style={{ textShadow: "1px 1px 2px #000000" }}
+            className="px-4 text-lg text-white hover:text-yellow-400 hover:shadow-lg active:text-orange-500"
           >
             Register
           </Link>
