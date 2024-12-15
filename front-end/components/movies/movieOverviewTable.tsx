285fdbf73e761bffd24bb6059499fface9502964
NielStroobantsUCLL <niel.stroobants@student.ucll.be>
2024-12-15 20:58:07 +0100

    movieOverviewtable fix

--- front-end/components/movies/movieOverviewTable.tsx
+++ front-end/components/movies/movieOverviewTable.tsx
@@ -1,38 +1,44 @@
-import React from 'react';
-import { Movie } from '@/types';
+import React from "react";
+import { Movie } from "@/types";
 
 type Props = {
-  movies: Array<Movie>;
+  movies: Array<Movie> | null | undefined; // Toegestane types voor robustness
 };
 
 const MovieOverviewTable: React.FC<Props> = ({ movies }: Props) => {
+  if (!Array.isArray(movies) || movies.length === 0) {
+    return <p>No movies available.</p>; // Fallback voor geen data
+  }
+
   return (
-    <>
-      {movies && (
-        <table className="table table-hover">
-          <thead>
-            <tr>
-              <th scope="col">Title</th>
-              <th scope="col">Genre</th>
-              <th scope="col">Duration</th>
-            </tr>
-          </thead>
-          <tbody>
-            {movies.map((movie, index) => (
-              <tr key={index}>
-                <td>{movie.name}</td>
-                <td>{movie.genre}</td>
-                <td>
-                  {typeof movie.duration === "string"
-                    ? new Date(movie.duration).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
-                    : movie.duration.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      )}
-    </>
+    <table className="table table-hover">
+      <thead>
+        <tr>
+          <th scope="col">Title</th>
+          <th scope="col">Genre</th>
+          <th scope="col">Duration</th>
+        </tr>
+      </thead>
+      <tbody>
+        {movies.map((movie, index) => (
+          <tr key={index}>
+            <td>{movie.name}</td>
+            <td>{movie.genre}</td>
+            <td>
+              {typeof movie.duration === "string"
+                ? new Date(movie.duration).toLocaleTimeString([], {
+                    hour: "2-digit",
+                    minute: "2-digit",
+                  })
+                : movie.duration.toLocaleTimeString([], {
+                    hour: "2-digit",
+                    minute: "2-digit",
+                  })}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 };
 
