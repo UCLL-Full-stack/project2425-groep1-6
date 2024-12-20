2acb6acb5d4a48ef6da270a9f9bb89a8ef802616
NielStroobantsUCLL <niel.stroobants@student.ucll.be>
2024-12-20 09:39:07 +0100

    Hardcoded versie voor buy ticket

--- front-end/services/ticketService.ts
+++ front-end/services/ticketService.ts
@@ -1,35 +1,34 @@
 import { Ticket } from "@/types";
 
 const getAllTickets = async () => {
-    return await fetch(`${process.env.NEXT_PUBLIC_API_URL}/tickets`, {
-      method: "GET",
-      headers: { "Content-Type": "application/json" }
-    });
-  };
-  
-  const getTicketById = async (ticketId: number) => {
-    return await fetch(`${process.env.NEXT_PUBLIC_API_URL}/tickets/${ticketId}`, {
-      method: "GET",
-      headers: { "Content-Type": "application/json" }
-    });
-  };
- 
-  const addTicket = async (ticket: Ticket) => {
-    return fetch(process.env.NEXT_PUBLIC_API_URL + "/ticket/addticket", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(ticket),
-    });
-  }
+  return await fetch(`${process.env.NEXT_PUBLIC_API_URL}/tickets`, {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+  });
+};
 
-  
-  const TicketService = {
-    getAllTickets,
-    getTicketById,
-    addTicket
-  };
-  
-  export default TicketService;
-  
+const getTicketById = async (ticketId: number) => {
+  return await fetch(`${process.env.NEXT_PUBLIC_API_URL}/tickets/${ticketId}`, {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+  });
+};
+
+const addTicket = async (ticket: Ticket, token: string) => {
+  return await fetch(`${process.env.NEXT_PUBLIC_API_URL}/ticket/addticket`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(ticket),
+  });
+};
+
+const TicketService = {
+  getAllTickets,
+  getTicketById,
+  addTicket,
+};
+
+export default TicketService;
